c58e177acb122155c3e555eccce350f5b8dd52b4
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-20 22:43:44 +0300

    All tasks done

---
+++ task032/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] GetArray(int size, int minValue, int maxValue)// Содаём массив и будем контролировать его рамер, мин. диапозон и макс. диапазон
+{
+    int[] result = new int[size];//создаём массив и в нём будет храниться число элиментов, какое мы решим через аргумент size
+    for (int i = 0; i < size; i++)//пробежимся по последовательности массива
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);//заполняем массив рандомными данными, диапазон котролируем сами. +1 т.к. он отмимет 1        
+    }
+    return result;
+}
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);// Сразу возвращаем данные, введённые пользаком
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }  
+    Console.WriteLine();  
+}
+
+int arrSize = GetNum("Задайте длинну массива: ");
+int arrMin = GetNum("Введите минимальное значение: ");
+int arrMax = GetNum("Введите максимальное значение: ");
+int[]arr = GetArray(arrSize,arrMin,arrMax);
+
+
+
+
+void NegativeArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = arr[i] * -1;
+    } 
+    Console.WriteLine("Противоположные значения вашего массива: ");
+    PrintArray(arr);   
+}
+
+PrintArray(arr);
+NegativeArray(arr);
---
+++ task033/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+Console.Clear();
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int GetNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void FindNumber(int[] array)
+{
+    int number = GetNum("Введите Искомое значение в массиве: ");
+    bool flag = false;// Правда или ложь. По умолчанию ЛОЖЬ
+    foreach (var el in array)
+    {
+        if (el == number) flag = true;// Если искомый номер есть в массиве, меняем на ПРАВДА
+    }
+    if (flag) Console.WriteLine("Да");// Если есть истина, то.... (Не обязательно писать Flag=true)
+    else Console.Write("Нет");
+}
+
+int arrSize = GetNum("Введите длинну массива: ");
+int arrMin = GetNum("Введите минимальное значение массива: ");
+int arrMax = GetNum("Введите максимальное значение массива: ");
+
+int[] arr = GetArray(arrSize, arrMin, arrMax);
+PrintArray(arr);
+
+FindNumber(arr);
